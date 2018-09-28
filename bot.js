46d95f2260765944261f37549d16a4d12bf735d7
denememexd <43650840+denememexd@users.noreply.github.com>
2018-09-28 16:10:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -100,7 +100,7 @@ client.unload = command => {
 };
 
 client.on('message', msg => {
-  if (msg.content.toLowerCase() === 'sa', 'Sa', 'SA', 'sA') {
+  if (msg.content.toLowerCase() === 'sa') {
     msg.channel.send('Aleyküm Selam,  Hoş Geldin');
   }
 });
